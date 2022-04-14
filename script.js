d1ea7414ff575b47d6ef71545c96dee90364c813
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-04-14 22:12:54 +0200

    Bug fixes

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
-const API_URL='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d48f331a6b75ea11ef5379196afb31c5&page1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d48f331a6b75ea11ef5379196afb31c5&page1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL='https://api.themoviedb.org/3/search/movie?api_key=d48f331a6b75ea11ef5379196afb31c5&query="';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=d48f331a6b75ea11ef5379196afb31c5&query="';
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('Search')
 
 //get initial data
 
-getMovies(API_URL) 
+getMovies(API_URL)
 
-async function getMovies(url){
+async function getMovies(url) {
     const res = await fetch(url)
-    const data = res.json()
-
+    const data = await res.json()
     showMovies(data.results)
 }
 
 
 
-function showMovies(movies){
+function showMovies(movies) {
     main.innerHTML = '';
-  
+
 
     movies.forEach((movie) => {
-        console.log(movie)
+        const movieEl = document.createElement('div');
 
-        const movieEl = document.createElement('div')
-        movieEl.classList.add( 'movie')
 
         movieEl.innerHTML = `
             <div class="movie">
-            <img src="${IMG_PATH + poster_path}">
+            <img src="${IMG_PATH + movie.poster_path}">
             
             <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getVote(vote_average)}">${vote-avarage}</span>
+                <h3>${movie.title}</h3>
+                <span class="${getVote(movie.vote_average)}">${movie.vote_average}</span>
             </div>
             <div class="overview">
                 <h3>overview</h3>
-                ${overview}
-            
+                ${movie.overview}
             </div>
-        
         </div>
         `
 
@@ -50,22 +45,23 @@ function showMovies(movies){
 
 }
 
-function getVote(vote){
-    if (vote >= 8){
+function getVote(vote) {
+    if (vote >= 8) {
         return 'green'
-    } else if(vote >= 5){
+    } else if (vote >= 5) {
         return 'orange'
-    }else{
+    } else {
         return 'red'
     }
 }
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const searchTerm = search.value;
-    if(searchTerm && searchTerm !== ''){
+    if (searchTerm && searchTerm !== '') {
         getMovies(SEARCH_URL + searchTerm)
-    }else{
+    } else {
         windows.location.reload()
     }
 })
